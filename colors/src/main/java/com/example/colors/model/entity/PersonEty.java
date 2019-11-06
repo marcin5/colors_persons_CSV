59fcf7b19d906421a7a12a85d5d4676b793d6bde
marcin5 <mar.moryl@gmail.com>
2019-11-06 10:45:42 +0100

    Delete jpa parts

--- colors/src/main/java/com/example/colors/model/entity/PersonEty.java
+++ colors/src/main/java/com/example/colors/model/entity/PersonEty.java
@@ -1,28 +1,18 @@
 package com.example.colors.model.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import com.example.colors.model.Color;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "Person")
 public class PersonEty {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   private String name;
   private String lastname;
   private Long zipcode;
